df57a9e9684e0ea815192ae78dc0cb8f350531db
ARCHANA <archanajayakumar374@gmail.com>
2024-12-24 22:00:50 +0530

    no

---
+++ assignment_2.py
@@ -0,0 +1,62 @@
+# # Exercise Q1: Create a list of 5 random numbers and print the list.
+# import random
+#
+# # Create a list of 5 random numbers
+# random_numbers = [random.randint(1, 100) for _ in range(5)]
+#
+# # # Print the list
+# # print("List of 5 random numbers:", random_numbers)
+# # # Exercise Q2: Insert 3 new values to the list and print the updated list.
+# # Inserting 3 new values to the list
+# # random_numbers.append(101)
+# # random_numbers.append(102)
+# # random_numbers.append(103)
+# #
+# # # Print the updated list
+# # print("Updated list after insertion:", random_numbers)
+# # Inserting 3 new values to the list
+# random_numbers.append(101)
+# random_numbers.append(102)
+# random_numbers.append(103)
+#
+# # Print the updated list
+# print("Updated list after insertion:", random_numbers)
+# Try to use a for loop to print each element in the list.
+# Using a for loop to print each element in the list
+# for num in random_numbers:
+#     print(num)
+# ##############Topic: Dictionary#######################################
+#Q1. Create a dictionary with keys 'name', 'age', and 'address' and values 'John', 25, and 'New York' respectively.
+# Creating a dictionary with specified keys and values
+# person_info = {
+#     'name': 'rithika',
+#     'age': 25,
+#     'address': 'kerala'
+# }
+#
+# print("Dictionary:", person_info)
+# # Q2. Add a new key-value pair to the dictionary created in Q1 with key 'phone' and value '1234567890'.
+# # Adding a new key-value pair to the dictionary
+# person_info['phone'] = '1234567890'
+#
+# print("Updated Dictionary:", person_info)
+#####################Topic: Set###################################
+# Creating a set with the specified values
+# my_set = {1, 2, 3, 4, 5}
+#
+# print("Set:", my_set)
+# # Adding the value 6 to the set
+# my_set.add(6)
+#
+# print("Set after adding 6:", my_set)
+# # Removing the value 3 from the set
+# my_set.remove(3)
+#
+# print("Set after removing 3:", my_set)
+#####################tupple##########################
+# Creating a tuple with specified values
+# my_tuple = (1, 2, 3, 4)
+#
+# print("Tuple:", my_tuple)
+# # Printing the length of the tuple
+# print("Length of the tuple:", len(my_tuple))
